1169b3ac92e2511f2cfebf09af2a4f2810cb5365
Dennis <github@farberg.de>
2016-06-03 10:06:27 +0200

    Moved messenger to another repo

--- src/main/java/jmsdemo/OpenWire.java
+++ src/main/java/jmsdemo/OpenWire.java
@@ -23,7 +23,7 @@ public class OpenWire {
 		// Create a connection
 		Connection connection = connectionFactory.createConnection();
 		connection.start();
-		log.debug("Started Connection...");
+		log.debug("Started Connection...: " + connection.getMetaData().getJMSProviderName());
 
 		// Create a session
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
